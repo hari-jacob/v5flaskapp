9fd8f5a1fdc152c7b1b0f3920dfb841c31f27bec
hari-jacob <49227425+hari-jacob@users.noreply.github.com>
2021-03-29 16:56:12 +0530

    Update testgroovy.groovy

--- testgroovy.groovy
+++ testgroovy.groovy
@@ -6,6 +6,10 @@ pipeline {
     //- update your credentials ID after creating credentials for connecting to Docker Hub
     registryCredential = 'harinathdockerid'
     dockerImage = ''
+    PROJECT_ID = 'tasko-task'
+    CLUSTER_NAME = 'k8s-cluster'
+    LOCATION = 'us-central1-c'
+    CREDENTIALS_ID = 'tasko-task'
   }
   stages {
     stage ('Code Pull') {
@@ -99,5 +103,17 @@ pipeline {
         }
       }
     }
+    stage('Deploy to K8s') {
+      steps{
+        echo "Deployment started ..."
+        sh 'ls -ltr'
+			  sh 'pwd'
+			  sh "sed -i 's/tagversion/${env.BUILD_ID}/g' deployment.yaml"
+        /*step([$class: 'KubernetesEngineBuilder', projectId: env.PROJECT_ID, clusterName: env.CLUSTER_NAME, location: env.LOCATION, manifestPattern: 'serviceLB.yaml', credentialsId: env.CREDENTIALS_ID, verifyDeployments: true])*/
+				echo "Start deployment of deployment.yaml"
+				step([$class: 'KubernetesEngineBuilder', projectId: env.PROJECT_ID, clusterName: env.CLUSTER_NAME, location: env.LOCATION, manifestPattern: 'deployment.yaml', credentialsId: env.CREDENTIALS_ID, verifyDeployments: true])
+			  echo "Deployment Finished ..."
+		    }
+	    }
   }
 }
